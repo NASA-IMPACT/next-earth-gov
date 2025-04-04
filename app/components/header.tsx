a9fb711d09c1edf6fd428089c34c214b2d9b617f
snmln <smalone@element84.com>
2025-04-04 17:09:01 -0400

    initial commit

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,6 +2,7 @@
 // @ts-nocheck : until veda-ui fixes its types: NavItem type enum
 import React from 'react';
 import { PageHeader } from '@lib';
+
 import { NavItem } from '@lib';
 import NasaLogoColor from 'app/components/nasa-logo-color.js';
 import {
@@ -10,61 +11,106 @@ import {
   STORY_HUB_PATH,
 } from 'app/config';
 import VedaUIConfigProvider from 'app/store/providers/veda-ui-config';
+import './earth-gov.scss';
 
 export const navItems: NavItem[] = [
   {
-    id: 'data-catalog',
-    title: 'Data Catalog',
+    id: 'about',
+    title: 'About',
     to: `/${DATASET_CATALOG_PATH}`,
     type: 'internalLink',
   },
+
   {
-    id: 'exploration',
-    title: 'Exploration',
+    id: 'themes',
+    title: 'Themes',
+    to: `/${STORY_HUB_PATH}`,
+    type: 'dropdown',
+    children: [
+      {
+        id: 'agriculture',
+        title: 'Agriculture',
+        to: '/stories/agriculture',
+        type: 'internalLink',
+      },
+      {
+        id: 'air-quality',
+        title: 'Air Quality',
+        to: '/stories/airquality',
+        type: 'internalLink',
+      },
+      {
+        id: 'bio-diversity',
+        title: 'Bio Diversity',
+        to: '/stories/biodiversity',
+        type: 'internalLink',
+      },
+      {
+        id: 'disasters',
+        title: 'Disasters',
+        to: '/stories/disasters',
+        type: 'internalLink',
+      },
+      {
+        id: 'energy',
+        title: 'Energy',
+        to: '/stories/energy',
+        type: 'internalLink',
+      },
+      {
+        id: 'greenhouse-gases',
+        title: 'Greenhouse Gases',
+        to: '/stories/greenhouse_gases',
+        type: 'internalLink',
+      },
+      {
+        id: 'sea-level-change',
+        title: 'Sea Level Change',
+        to: '/stories/sea-level-change',
+        type: 'internalLink',
+      },
+      {
+        id: 'wildfires',
+        title: 'Wildfires',
+        to: '/stories/wildfires',
+        type: 'internalLink',
+      },
+      {
+        id: 'water-resources',
+        title: 'Water Resources',
+        to: '/stories/water-resources',
+        type: 'internalLink',
+      },
+    ],
+  },
+  {
+    id: 'dashboard',
+    title: 'Dashboard',
     to: `/${EXPLORATION_PATH}`,
     type: 'internalLink',
   },
-  {
-    id: 'stories',
-    title: 'Stories',
-    to: `/${STORY_HUB_PATH}`,
-    type: 'internalLink',
-  },
 ];
-
 export const subNavItems: NavItem[] = [
-  {
-    id: 'about',
-    title: 'About',
-    to: '/about',
-    type: 'internalLink',
-  },
-  {
-    id: 'contact-us',
-    title: 'Contact us',
-    actionId: 'open-google-form',
-    type: 'action',
-  },
+  // {
+  //   id: 'about',
+  //   title: 'About',
+  //   to: '/about',
+  //   type: 'internalLink',
+  // },
+  // {
+  //   id: 'contact-us',
+  //   title: 'Contact us',
+  //   actionId: 'open-google-form',
+  //   type: 'action',
+  // },
 ];
-
 export default function Header() {
   return (
     <VedaUIConfigProvider>
       <PageHeader
-        title={'Earthdata VEDA Dashboard'}
+        title={'Earth.gov'}
         mainNavItems={navItems}
         subNavItems={subNavItems}
-        logoSvg={
-          <div id='logo-container-link'>
-            {/*
-              USWDS targets only <a> tags for styling links. However when the text is a <span>
-              instead of a link, it does not inherit the color styling (it ends up being white).
-              To fix this, we must add the color inline like this.
-              TODO: Ideally we can address this on the veda-ui side so that the color applies to all elements within the logo.
-            */}
-            <NasaLogoColor />
-          </div>
-        }
       />
     </VedaUIConfigProvider>
   );
