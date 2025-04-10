61eddbdee05c519c83ca30836afe3d326ca183b2
Alice Rühl <alice@developmentseed.org>
2025-04-10 12:37:05 +0200

    Attach scan results to test report

--- test/integration/a11y.spec.ts
+++ test/integration/a11y.spec.ts
@@ -4,10 +4,15 @@ import AxeBuilder from '@axe-core/playwright';
 test.describe('homepage', () => {
   test('should not have any automatically detectable accessibility issues', async ({
     page,
-  }) => {
+  }, testInfo) => {
     await page.goto('/');
 
     const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    await testInfo.attach('accessibility-scan-results', {
+      body: JSON.stringify(accessibilityScanResults, null, 2),
+      contentType: 'application/json',
+    });
 
     expect(accessibilityScanResults.violations).toEqual([]);
   });
@@ -16,10 +21,15 @@ test.describe('homepage', () => {
 test.describe('about page', () => {
   test('should not have any automatically detectable accessibility issues', async ({
     page,
-  }) => {
+  }, testInfo) => {
     await page.goto('/about');
 
     const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    await testInfo.attach('accessibility-scan-results', {
+      body: JSON.stringify(accessibilityScanResults, null, 2),
+      contentType: 'application/json',
+    });
 
     expect(accessibilityScanResults.violations).toEqual([]);
   });
