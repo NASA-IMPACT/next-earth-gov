5d423f3319d5b1435630831c049781225e51c964
Alice Rühl <alice@developmentseed.org>
2025-04-10 12:28:57 +0200

    Add a11y checks for home and about page

---
+++ test/integration/a11y.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
+
+test.describe('homepage', () => {
+  test('should not have any automatically detectable accessibility issues', async ({
+    page,
+  }) => {
+    await page.goto('/');
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+});
+
+test.describe('about page', () => {
+  test('should not have any automatically detectable accessibility issues', async ({
+    page,
+  }) => {
+    await page.goto('/about');
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+});
