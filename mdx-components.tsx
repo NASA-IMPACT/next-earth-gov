2c856cac1a14c1c3583b178a2224cc3531607f27
Vitor George <vitor.george@gmail.com>
2025-04-11 15:10:02 +0100

    feat: add about page content

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,11 @@
 import type { MDXComponents } from 'mdx/types';
 
+import { Block, Prose } from '@lib';
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
+    Block,
+    Prose,
   };
 }
